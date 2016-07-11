7671c39fe433cb2534052524b5de606e5b34a5f6
HariHaran <hariharanvasphi@gmail.com>
2016-07-11 21:20:50 +0530

    Add files via upload

---
+++ stringtoint.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class stringtoint{
+public static void main(String args[]){
+String a;
+int n;
+Scanner sc=new Scanner(System.in);
+a=sc.nextLine();
+n=Integer.parseInt(a);
+System.out.println(n);
+}
+}
