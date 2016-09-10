c9990327c60994e843f04d0ce86243d4c510cac9
HariHaran <hariharanvasphi@gmail.com>
2016-09-10 15:09:40 +0530

    Create unique.java

---
+++ unique.java
@@ -0,0 +1,41 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class unique
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,i;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int []a=new int[n];
+		for(i=0;i<n;i++){
+			a[i]=sc.nextInt();
+			
+		}
+		int []b=new int[n];
+			for(i=0;i<n;i++){
+			b[i]=0;
+			}
+			for(i=0;i<n;i++){
+				
+				b[a[i]]++;
+			}
+			
+		for(i=0;i<n;i++){
+			if(b[i]==1){
+				System.out.println(a[i]);
+			
+			}
+			/*else{
+						System.out.println(a[i]);
+			}*/
+		}
+		
+	}
+}
