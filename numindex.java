633d3816e9dbeb008457627986aeb645b601d065
HariHaran <hariharanvasphi@gmail.com>
2016-09-10 14:09:20 +0530

    Create numindex.java

---
+++ numindex.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.util.Scanner;
+class numindex
+{
+public static void main(String args[])
+{
+int i,j;
+int aa[]=new int[5];
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the array in ascending order:");
+ for(i=0;i<5;i++)
+a[i]=sc.nextInt();
+ System.out.println("Array elements are : ");
+        for (j=0;j<aa.length;j++)
+            System.out.println(a[j]);
+System.out.println("enter any number from the array:");
+int n=sc.nextInt();
+
+if(n==aa[n])
+System.out.println("number is"+n);
+else
+System.out.println("number is not equal to index");
+}
+}
